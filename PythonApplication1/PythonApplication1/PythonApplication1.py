da695741a727bac81216919acfc891d1e0bacd62
Simion Petrov <simion.petrov@hotmail.com>
2018-01-09 23:12:21 +0200

    imported the numpy package and changed the environment to python 3.6

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,5 +1,12 @@
 
+import math
 print ("Hello World")
+print(4+5)
+r = 0.43
+C = 2*math.pi*r
+A = 2*math.pi*r**2
+print(C)
+print(A)
 #we are adding our first commets onour first branch
 #ha ha ha ha 
 #branch number 4
